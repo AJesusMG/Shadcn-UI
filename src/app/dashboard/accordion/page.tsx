ec27fa134d9f1b2aab902baad4f0be47f8574c28
Jesus Guzman <amorales130@alumnos.uaq.mx>
2024-10-18 11:12:21 -0600

    Feat: Added the alert and accordion component

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,8 +1,39 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+const items = [
+  {
+    id: "item-1",
+    question: " Is it accessible?",
+    answer: "Yes. It adheres to the  WAI-ARIA desing pattern.",
+  },
+  {
+    id: "item-2",
+    question: " Is it accessible?",
+    answer: "Yes. It adheres to the  WAI-ARIA desing pattern.",
+  },
+  {
+    id: "item-3",
+    question: " Is it accessible?",
+    answer: "Yes. It adheres to the  WAI-ARIA desing pattern.",
+  },
+];
 
 export default function Page() {
   return (
     <div>
-      <h1>Accordion Page</h1>
+      <Accordion type="single" className="w-full">
+        {items.map((item) => (
+          <AccordionItem value={item.id} key={item.id}>
+            <AccordionTrigger>{item.question}</AccordionTrigger>
+            <AccordionContent>{item.answer}</AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </div>
   );
 }
